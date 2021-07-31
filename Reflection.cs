8323e308a2b84d8665564f8eaa39bf572d69c369
kiranravindrant <kiranravindrant@gmail.com>
2021-07-31 20:57:51 +0530

    [kiran]Added Class Reflection

---
+++ Reflection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+        
+namespace RegexUserRegistration
+{
+    class Reflection
+    {
+        public static void ReflectionTest()
+        {
+
+            Type type = Type.GetType("RegexUserRegistration.Program");
+            Console.WriteLine("Full Name :{0}", type.FullName);
+            Console.WriteLine("Class name is : {0}", type.Name);
+            Console.WriteLine("Methods in Program class");
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+            Console.WriteLine("Properties in Program class");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+
+            Console.WriteLine("Constructors in Program class");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+
+        }
+
+
+    }
+}
